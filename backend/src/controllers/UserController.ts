a7c273c9d0fb98882a8ba6edebed81ba31420151
Alan Guerra <alanlimadasilvaguerra23@gmail.com>
2024-04-08 15:03:23 -0300

    criando controle para envio de email

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
   {
@@ -10,5 +11,16 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users)
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail(
+      { email: 'alanguerra@gmail.com', name: 'Alan Guerra' }, 
+      { subject: 'Bem Vindo', body: 'Boas Vindas!' }
+    );
+
+    return res.send()
   }
 };
