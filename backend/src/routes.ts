03fba038f82ce133f68ace4f0da62c695ac6660e
Alan Guerra <alanlimadasilvaguerra23@gmail.com>
2024-04-08 15:03:56 -0300

    consumindo controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,5 +4,6 @@ import UserController from './controllers/UserController';
 const routes = Router();
 
 routes.get('/users', UserController.index);
+routes.get('/user/create', UserController.create);
 
 export default routes;
